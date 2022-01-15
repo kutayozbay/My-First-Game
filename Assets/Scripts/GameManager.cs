295567cf586191eb276744e274811a642d41c80c
kutayozbay <kutayozbay00@gmail.com>
2022-01-15 15:47:19 +0300

    code

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gamehasEnded = false;
+    public GameObject completeLevelUI;
+    public float restartDelay = 2f;
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+    public void EndGame()
+    {
+        if (gamehasEnded==false)
+        {
+            gamehasEnded = true;
+            Debug.Log("GAMEOVER");
+            Invoke("Restart", time: restartDelay);
+        }
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+}
